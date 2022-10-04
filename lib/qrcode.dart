93596e8f11d70fd2c02b53d9be60daa7f42d0888
Xavier Hainaux <xavier.hainaux@gmail.com>
2022-10-04 13:51:37 +0200

    Order exports

--- lib/qrcode.dart
+++ lib/qrcode.dart
@@ -1,7 +1,7 @@
+export 'src/encode_hint.dart';
 export 'src/qrcode/decoder/error_correction_level.dart'
     show ErrorCorrectionLevel;
 export 'src/qrcode/encoder/encoder.dart' show Encoder;
 export 'src/qrcode/encoder/qr_code.dart' show QRCode;
 export 'src/qrcode/qrcode_reader.dart' show QRCodeReader;
-export 'src/encode_hint.dart';
 export 'zxing2.dart';
